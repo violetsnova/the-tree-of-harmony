6dd588b82ee3e9f79d4976a66077105ab137855e
violetsnova <violetcsnova@gmail.com>
2024-06-07 03:46:13 -0500

    changed the symbol to an image (needs fixing)
    currently doesnt work because i have the wrong path. will fix

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("h", {
     name: "Honesty", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "h", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: '<img src="GitHub\the-unnamed-tree\resources\honesty.png" />', // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
